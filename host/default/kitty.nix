1c7ab2d6f95b1c794dcb4749d285cff62bd67009
drs <onsun99@gmail.com>
2026-04-26 16:51:27 +0200

    mi primer commit

---
+++ host/default/kitty.nix
@@ -0,0 +1,26 @@
+{ config, pkgs, ... }:
+
+{
+  programs.kitty = {
+    enable = true;
+    
+    # Configuración de fuente (opcional)
+    font = {
+      name = "JetBrains Mono";
+      size = 12;
+    };
+    
+    # Tema (opcional)
+    themeFile = "1984_dark";
+    
+    settings = {
+      # Aquí solo pones las opciones que quieres personalizar.
+      # Las opciones que no escribas, Kitty usará sus valores por defecto.
+      # Añade esta línea para que el cursor parpadee.
+      cursor_blink_interval = "0.5"; 
+      
+      # Elimina todas las demás líneas que tenía, como:
+      # background_opacity, window_padding_width, etc.
+    };
+  };
+}
---
+++ host/default/window-managers/hyprland.nix
@@ -0,0 +1,12 @@
+{config, pkgs, ...}:
+{
+
+wayland.windowManager.hyprland = {
+
+
+
+
+};
+
+
+}
