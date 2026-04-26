fcdddff5d850dd85b703660f08c860c0d1b2d069
drs <onsun99@gmail.com>
2026-04-26 18:29:01 +0200

    add hyprland

--- host/default/configuration.nix
+++ host/default/configuration.nix
@@ -42,7 +42,7 @@ nix.settings.experimental-features = ["nix-command" "flakes"];
 programs.localsend.enable = true;
 services.xserver.enable = true;
 services.displayManager.gdm.enable = true;
-programs.hyprland.enable = true;
+#programs.hyprland.enable = true;
 services.flatpak.enable = true;
 
   # Configure network proxy if necessary
@@ -110,9 +110,8 @@ home-manager = {
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
-	hyprland
+	#hyprland
 	git
-	#kitty
 	wofi
 	
 	firefox
--- host/default/home.nix
+++ host/default/home.nix
@@ -10,6 +10,7 @@ in
 imports = [
     ./kitty.nix
     ./waybar.nix
+    ./hyprland.nix
   ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
