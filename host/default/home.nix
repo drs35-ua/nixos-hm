5799520087da6846e15c3508bc07534c8be0da17
drs <onsun99@gmail.com>
2026-04-26 21:17:53 +0200

    modo oscuro

--- host/default/home.nix
+++ host/default/home.nix
@@ -59,7 +59,12 @@ imports = [
     xdg-user-dirs-gtk
   ];
 
-
+dconf.settings = {
+  "org/gnome/desktop/interface" = {
+    color-scheme = "prefer-dark";
+    gtk-theme = "Adwaita-dark";  # o el tema oscuro que uses
+  };
+};
 
     # Configuración de Bash (manera correcta en Home Manager actual)
   # Configuración de Bash
