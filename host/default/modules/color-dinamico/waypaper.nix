c5ac6aa68728333f3ada33d01925e5f643f413b7
drs35-ua <onsun99@gmail.com>
2026-04-29 19:00:19 +0200

    waypaper rutas mas o menos relativas

--- host/default/modules/color-dinamico/waypaper.nix
+++ host/default/modules/color-dinamico/waypaper.nix
@@ -1,13 +1,11 @@
 { config, pkgs, ... }:
 
 let
-  # Ruta absoluta al archivo real (dentro de tu flake)
-  realConfig = "/home/dani/.nixflakes/host/default/etc/waypaper-config.ini";
+  realConfig = "${config.home.homeDirectory}/.nixflakes/host/default/etc/waypaper-config.ini";
 in
 {
-  # Usa home.file en lugar de xdg.configFile si quieres más control
   home.file.".config/waypaper/config.ini" = {
     source = config.lib.file.mkOutOfStoreSymlink realConfig;
-    force = true;   # Sobrescribe si ya existe
+    force = true;
   };
 }
